56691da9932d1b89a0ab781e05d13b91443aa135
EmmanuelBrownn <okonemmanuel3119@gmail.com>
2023-06-18 18:19:03 -0700

    cold

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,17 +1,29 @@
+#include <stdlib.h>
 #include <stdio.h>
+#include <time.h>
+
 /**
- * main - prints the alphabet in lowercase,
- * followed by a new line
- * Return: Always 0 (Success)
- */
+* main - Entry poiter
+*
+* Decription : print digit of number stored in the variable n.
+*
+* Return: Alwyas 0 (Success)
+*/
+
 int main(void)
 {
-	char ch;
+	int n, num;
 
-  	for (ch = 'a'; ch <= 'z'; ch++)
-{
-	putchar(ch);
-}
-	putchar('\n');
-	return (0);
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	num = n % 10;
+
+	/*My code for Last digit number*/
+	if (num > 5)
+		printf("Last digit of %d is %d and is greater than 5\n", n, num);
+	else if (num < 6 && num != 0)
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, num);
+	else
+		printf("Last digit of %d is %d and is 0\n", n, num);
+return (0);
 }
